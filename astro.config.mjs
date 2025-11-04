5dc43577e043020af51c9c2b4295dcee4f565c42
thisismattsmith <hello@thisismattsmith.com>
2025-11-04 18:37:06 +1100

    Reveted file, installed netlify adaptor and added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,20 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.thisismattsmith.com",
-	integrations: [
-		mdx(),
-		sitemap({
-			changefreq: "daily",
-			priority: 1,
-			lastmod: new Date().toISOString().split("T")[0],
-		}),
-		tailwind(),
-	],
-	output: "server",
-	adapter: cloudflare(),
+    site: "https://www.thisismattsmith.com",
+    integrations: [
+        mdx(),
+        sitemap({
+            changefreq: "daily",
+            priority: 1,
+            lastmod: new Date().toISOString().split("T")[0],
+        }),
+        tailwind(),
+    ],
+    output: "server",
+    adapter: netlify(),
 });
