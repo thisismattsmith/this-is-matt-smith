d725f5c8156a7bba8dced66842732a3f9ea02306
thisismattsmith <hello@thisismattsmith.com>
2025-11-07 06:40:13 +1100

    Resolving conflicts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -26,13 +26,13 @@ export function getCategoryColor(category: string): string {
       return "bg-lime-300";
     case "product":
       return "bg-green-300";
-    case "engineering":
+    case "consulting":
       return "bg-purple-300";
     case "productivity":
       return "bg-pink-300";
     case "learning":
       return "bg-blue-300";
-    case "opensource":
+    case "self-employment":
       return "bg-orange-400";
     case "thoughts":
       return "bg-red-400";
